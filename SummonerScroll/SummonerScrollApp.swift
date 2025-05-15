ad194453e0a1de203089f47f109c4f3cc44493a6
SDV Bordeaux <sdvbordeaux@MBPBX0017.local>
2025-05-15 10:07:02 +0200

    Initial commit for xcode
    Using tests

---
+++ SummonerScroll/SummonerScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummonerScrollApp.swift
+//  SummonerScroll
+//
+//  Created by SDV Bordeaux on 15/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct SummonerScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SummonerScrollTests/SummonerScrollTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummonerScrollTests.swift
+//  SummonerScrollTests
+//
+//  Created by SDV Bordeaux on 15/05/2025.
+//
+
+import Testing
+@testable import SummonerScroll
+
+struct SummonerScrollTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
